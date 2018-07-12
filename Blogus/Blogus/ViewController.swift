46c84972709e63d17818cb2e2ba8d9cf4390a257
Puskuruk <tamayeseruysal@gmail.com>
2018-07-12 16:57:49 +0300

    Made Blog To App

---
+++ Blogus/Blogus/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Blogus
+//
+//  Created by Lab2023 Stajyer on 12.07.2018.
+//  Copyright © 2018 Lab2023 Stajyer. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://puskuruk.github.io")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
